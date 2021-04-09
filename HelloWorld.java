e06a3ab4c1583c004f6e5bc94fcdc357b07a7dd2
Rohan <rohan.bisht@apisero.com>
2021-04-09 14:07:50 +0530

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+System.out.println("Namastey");
+System.out.println("Hi");
+System.out.println("Yo");
+System.out.println("Heys");
+System.out.println("Bruh");
+     }
+}
